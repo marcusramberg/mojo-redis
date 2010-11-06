00e025d9b4a5972a026412e5268e2d3c18d3b540
Sergey Zasenko <d3fin3@gmail.com>
2010-11-06 16:11:29 +0200

    Used perltidy

--- lib/MojoX/Redis.pm
+++ lib/MojoX/Redis.pm
@@ -9,32 +9,34 @@ use base 'Mojo::Base';
 use Mojo::IOLoop;
 use List::Util ();
 
-__PACKAGE__->attr( server => '127.0.0.1:6379' );
-__PACKAGE__->attr( ioloop => sub { Mojo::IOLoop->singleton } );
-__PACKAGE__->attr( error  => undef );
-__PACKAGE__->attr( timeout=> 300 );
+__PACKAGE__->attr(server  => '127.0.0.1:6379');
+__PACKAGE__->attr(ioloop  => sub { Mojo::IOLoop->singleton });
+__PACKAGE__->attr(error   => undef);
+__PACKAGE__->attr(timeout => 300);
 
 sub connect {
     my $self = shift;
 
     # drop old connection
-    if ( $self->{_connection} ) {
-        $self->ioloop->drop( $self->{_connection} );
+    if ($self->{_connection}) {
+        $self->ioloop->drop($self->{_connection});
     }
 
-    $self->server=~m{^([^:]+)(:(\d+))?};
+    $self->server =~ m{^([^:]+)(:(\d+))?};
     my $address = $1;
-    my $port    = $3 || 6379;
+    my $port = $3 || 6379;
+
     # connect
     $self->{_connecting} = 1;
-    $self->{_connection} = $self->ioloop->connect({
-        address    => $address,
-        port       => $port,
-        on_connect => sub { $self->_on_connect(@_) },
-        on_read    => sub { $self->_read_wait_command(@_)    },
-        on_error   => sub { $self->_on_error(@_)   },
-        on_hup     => sub { $self->_on_hup(@_)     },
-    });
+    $self->{_connection} = $self->ioloop->connect(
+        {   address    => $address,
+            port       => $port,
+            on_connect => sub { $self->_on_connect(@_) },
+            on_read    => sub { $self->_read_wait_command(@_) },
+            on_error   => sub { $self->_on_error(@_) },
+            on_hup     => sub { $self->_on_hup(@_) },
+        }
+    );
 
     return $self;
 }
@@ -42,30 +44,30 @@ sub connect {
 sub execute {
     my ($self, $command, $args, $cb) = @_;
 
-    if ( !$cb && ref $args eq 'CODE' ) {
+    if (!$cb && ref $args eq 'CODE') {
         $cb   = $args;
         $args = [];
-    } elsif ( ! ref $args ) {
-        $args = [ $args ];
+    }
+    elsif (!ref $args) {
+        $args = [$args];
     }
 
     unshift @$args, uc $command;
 
     my $message = '*' . scalar(@$args) . "\r\n";
     foreach my $token (@$args) {
-        $message .= '$' . length($token) . "\r\n"
-                    . "$token\r\n";
-                    
+        $message .= '$' . length($token) . "\r\n" . "$token\r\n";
+
     }
     $message .= "\r\n";
 
     my $queue = $self->{_queue} ||= [];
 
-    push @$queue, [ $message, $cb ];
+    push @$queue, [$message, $cb];
 
     $self->connect unless $self->{_connection};
     $self->_send_next_message;
-    
+
     return $self;
 }
 
@@ -74,10 +76,10 @@ sub _send_next_message {
 
     $self->{_queue}    ||= [];
     $self->{_cb_queue} ||= [];
-    if ( (my $c = $self->{_connection}) && !$self->{_connecting} ) {
-        foreach my $msg ( @{$self->{_queue}} ) {
+    if ((my $c = $self->{_connection}) && !$self->{_connecting}) {
+        foreach my $msg (@{$self->{_queue}}) {
             my ($message, $cb) = @$msg;
-            $self->ioloop->write( $c, $message );
+            $self->ioloop->write($c, $message);
             push @{$self->{_cb_queue}}, $cb;
         }
     }
@@ -87,7 +89,7 @@ sub _on_connect {
     my ($self, $ioloop, $id) = @_;
     delete $self->{_connecting};
 
-    $ioloop->connection_timeout( $id => $self->timeout );
+    $ioloop->connection_timeout($id => $self->timeout);
 
     $self->_send_next_message;
 }
@@ -96,15 +98,15 @@ sub _return_command_data {
     my ($self, @data) = @_;
 
     my $cb = shift @{$self->{_cb_queue}};
-    $cb->( @data ) if $cb;
+    $cb->(@data) if $cb;
 
-    $self->error( undef );
+    $self->error(undef);
 }
 
 sub _on_error {
     my ($self, $ioloop, $id, $error) = @_;
 
-    $self->error( $error );
+    $self->error($error);
     $self->_inform_queue;
 }
 
@@ -118,7 +120,7 @@ sub _on_hup {
 sub _inform_queue {
     my ($self) = @_;
 
-    for my $cb ( @{$self->{_cb_queue}} ) {
+    for my $cb (@{$self->{_cb_queue}}) {
         $cb->() if $cb;
     }
     $self->{_queue} = [];
@@ -128,31 +130,52 @@ sub _read_wait_command {
     my ($self, $ioloop, $id, $chunk) = @_;
 
     my $cmd = substr $chunk, 0, 1, '';
-    if ( !defined $chunk || $chunk eq '' ) {
+    if (!defined $chunk || $chunk eq '') {
+
         # Wait next command
-        $ioloop->on_read( $id => sub { $self->_read_wait_command( @_ ) } );
+        $ioloop->on_read($id => sub { $self->_read_wait_command(@_) });
     }
-    elsif ( List::Util::first { $cmd eq $_ } ('+', '-', ':') ) {
+    elsif (List::Util::first { $cmd eq $_ } ('+', '-', ':')) {
+
         # Just a simple one line command
         $self->{_read_cmd_string} = '';
-        if ( $cmd ne '-' ) {
-            $self->{_read_cb} = sub { $self->_return_command_data( shift ); $self->_read_wait_command( $ioloop, $id, shift );  };
-        } else {
-            $self->{_read_cb} = sub { $self->error(shift->[0]); $self->_return_command_data(undef); $self->_read_wait_command( $ioloop, $id, shift );  };
+        if ($cmd ne '-') {
+            $self->{_read_cb} = sub {
+                $self->_return_command_data(shift);
+                $self->_read_wait_command($ioloop, $id, shift);
+            };
+        }
+        else {
+            $self->{_read_cb} = sub {
+                $self->error(shift->[0]);
+                $self->_return_command_data(undef);
+                $self->_read_wait_command($ioloop, $id, shift);
+            };
         }
 
-        $ioloop->on_read( $id => sub { $self->_read_string_command( @_ ); } );
-        $self->_read_string_command( $ioloop, $id, $chunk );
+        $ioloop->on_read($id => sub { $self->_read_string_command(@_); });
+        $self->_read_string_command($ioloop, $id, $chunk);
 
-    } elsif ( $cmd eq '$' ) {
+    }
+    elsif ($cmd eq '$') {
+
         # Bulk command, not a big deal
-        $self->{_read_cb} = sub { $self->_return_command_data(shift); $self->_read_wait_command( $ioloop, $id, shift );  };
+        $self->{_read_cb} = sub {
+            $self->_return_command_data(shift);
+            $self->_read_wait_command($ioloop, $id, shift);
+        };
+
         # Yes, it should have leading $
-        $self->_read_bulk_command( $ioloop, $id, "\$$chunk" );
-    } elsif ( $cmd eq '*' ) {
-        $self->{_read_cb} = sub { $self->_return_command_data(shift); $self->_read_wait_command( $ioloop, $id, shift );  };
-        $self->_read_multi_bulk_command( $ioloop, $id, $chunk );
-    } else {
+        $self->_read_bulk_command($ioloop, $id, "\$$chunk");
+    }
+    elsif ($cmd eq '*') {
+        $self->{_read_cb} = sub {
+            $self->_return_command_data(shift);
+            $self->_read_wait_command($ioloop, $id, shift);
+        };
+        $self->_read_multi_bulk_command($ioloop, $id, $chunk);
+    }
+    else {
         die qq{Strange input "$cmd$chunk"};
     }
 }
@@ -163,16 +186,17 @@ sub _read_string_command {
     my $str = $self->{_read_cmd_string} .= $chunk;
     my $i = index $str, "\r\n";
 
-    if ( $i >= 0 ) {
+    if ($i >= 0) {
+
         # Got full command
         my $result = substr $str, 0, $i, '';
-        substr $str, 0, 2, ''; # Delete \r\n
+        substr $str, 0, 2, '';    # Delete \r\n
 
         #print "## $result\n## $str\n";
         my $cb = $self->{_read_cb};
         delete $self->{_read_cb};
         delete $self->{_read_cmd_string};
-        $cb->( [$result], $str );
+        $cb->([$result], $str);
     }
 }
 
@@ -188,28 +212,31 @@ sub _read_multi_bulk_command {
     $mbulk_process = sub {
         push @$results, shift->[0];
 
-        if ( scalar @$results == $self->{_read_cmd_num}) {
+        if (scalar @$results == $self->{_read_cmd_num}) {
             $mbulk_process = undef;
             $mbulk_cb->($results, shift);
-        } else {
+        }
+        else {
+
             # Read another string
             #print "### Got another result: ", $results->[-1], "\n";;
             $self->{_read_cb} = $mbulk_process;
-            $self->_read_bulk_command( $ioloop, $id, shift );
+            $self->_read_bulk_command($ioloop, $id, shift);
         }
     };
 
     # Read number of commands
     $self->{_read_cb} = sub {
         $self->{_read_cmd_num} = shift->[0];
+
         #print "Got #commands: ", $self->{_read_cmd_num}, "\n";
         $self->{_read_cb} = $mbulk_process;
-        $ioloop->on_read( $id => sub { $self->_read_bulk_command( @_ ) } );
-        $self->_read_bulk_command( $ioloop, $id, shift );
+        $ioloop->on_read($id => sub { $self->_read_bulk_command(@_) });
+        $self->_read_bulk_command($ioloop, $id, shift);
     };
 
-    $ioloop->on_read( $id => sub { $self->_read_string_command( @_ ); } );
-    $self->_read_string_command( $ioloop, $id, $chunk );
+    $ioloop->on_read($id => sub { $self->_read_string_command(@_); });
+    $self->_read_string_command($ioloop, $id, $chunk);
 }
 
 sub _read_bulk_command {
@@ -222,33 +249,36 @@ sub _read_bulk_command {
     # Read size of string
     $self->{_read_cb} = sub {
         my $size = shift->[0];
+
         # Delete leading $
-        substr $size, 0, 1, ""; 
+        substr $size, 0, 1, "";
         $self->{_read_cmd_legth} = $size;
+
         #print "Got size: ", $self->{_read_cmd_legth}, "\n";
         $self->{_read_cb} = $bulk_cb;
-        $ioloop->on_read( $id => sub { $self->_read_bulk_command( @_ ) } );
-        $self->_read_bulk_command_string( $ioloop, $id, shift );
+        $ioloop->on_read($id => sub { $self->_read_bulk_command(@_) });
+        $self->_read_bulk_command_string($ioloop, $id, shift);
     };
 
-    $ioloop->on_read( $id => sub { $self->_read_string_command( @_ ); } );
-    $self->_read_string_command( $ioloop, $id, $chunk );
+    $ioloop->on_read($id => sub { $self->_read_string_command(@_); });
+    $self->_read_string_command($ioloop, $id, $chunk);
 }
 
 sub _read_bulk_command_string {
     my ($self, $ioloop, $id, $chunk) = @_;
 
     my $str = $self->{_read_cmd_string} .= $chunk;
+
     #print "String $str\n";
-    if ( length $str >= $self->{_read_cmd_legth} ) {
+    if (length $str >= $self->{_read_cmd_legth}) {
         my $result = substr $str, 0, $self->{_read_cmd_legth}, "";
-        substr $str, 0, 2, ""; # Delete \r\n
+        substr $str, 0, 2, "";    # Delete \r\n
 
         #print "## str result $result\n";
         my $cb = $self->{_read_cb};
         delete $self->{_read_cb};
         delete $self->{_read_cmd_string};
-        $cb->( [$result], $str );
+        $cb->([$result], $str);
     }
 }
 
