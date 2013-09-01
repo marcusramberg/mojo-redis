a3dd764dc6b88100268ee4e82db0f4e817301067
Jan Henning Thorsen <jhthorsen@cpan.org>
2013-09-01 11:01:10 +0200

    Fix leaking into protocol_redis
      But t/leak.t still fail. (It is also failing in 0.9913)
    
      REDIS_SERVER=127.0.0.1:6379 prove -vl t/leak.t
      t/leak.t ..
      1..4
      not ok 1 - ping (leaks 9 <= 0)
    
      #   Failed test 'ping (leaks 9 <= 0)'
      #   at t/leak.t line 29.
      #     '9'
      #         <=
      #     '0'
      # leaked HASH(0x27a0398) from /usr/lib/perl/5.14/IO/Poll.pm line 75.
      #   74:
      #   75:    $self->[1] = {};
      #   76:
      # SV = PVHV(0x282eb00) at 0x27a0398
      #   REFCNT = 1
      #   FLAGS = (OOK,SHAREKEYS)
      #   ARRAY = 0x2836440
      #   KEYS = 0
      #   FILL = 0
      #   MAX = 7
      #   RITER = -1
      #   EITER = 0x0
      ok 2 - error (leaks 0 <= 0)
      ok 3 - set (leaks 0 <= 0)
      ok 4 - get (leaks 0 <= 0)
      # Looks like you failed 1 test of 4.
      Dubious, test returned 1 (wstat 256, 0x100)
      Failed 1/4 subtests
    
      Test Summary Report
      -------------------
      t/leak.t (Wstat: 256 Tests: 4 Failed: 1)
        Failed test:  1
        Non-zero exit status: 1
      Files=1, Tests=4,  1 wallclock secs ( 0.03 usr  0.00 sys +  0.39 cusr  0.01 csys =  0.43 CPU)
      Result: FAIL

--- lib/Mojo/Redis.pm
+++ lib/Mojo/Redis.pm
@@ -26,6 +26,7 @@ has protocol => sub {
   my $protocol = $self->protocol_redis->new(api => 1);
 
   $protocol or Carp::croak(q/Protocol::Redis implementation doesn't support APIv1/);
+  Scalar::Util::weaken($self);
   $protocol->on_message(
     sub {
       my ($parser, $command) = @_;
